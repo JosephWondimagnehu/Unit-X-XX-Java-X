595d1c49d146101d02961b83f6633bc0f8bcf67b
JosephWondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-09-04 10:46:14 -0400

    Update Template.java

--- Template.java
+++ Template.java
@@ -1,1 +1,13 @@
+/**
+* Explain what your program does.
+*
+* @author  Joseph Wondimagnehu
+* @version 1.0
+* @since   Year-Month-Day
+*/
 
+// Creates the container for the program.
+class Main {
+  // Defines the entry point of the program.
+  public static void main(String[] args) {}
+}
